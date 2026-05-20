6d0ff1db8dfa171a608e139bfdf37ddaac3575a8
pkumar1618 <prashanthch943@gmail.com>
2026-05-20 17:59:05 +0530

    21-redirectors with Usage

--- 21-redirectors-usage.sh
+++ 21-redirectors-usage.sh
@@ -16,7 +16,7 @@ CHECK_ROOT() {
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then 
-        echo -e "$2 ... $R FAILED $N" | tee -a "$LOG_FILE"
+        echo -e "$2 ... $R FAILED $N" | tee -a "$LOG_FILE" # tee command using for write logs on terminal and log file both 
         exit 1
     else
         echo -e "$2 ... $G SUCCESS $N" | tee -a "$LOG_FILE"
