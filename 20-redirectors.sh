22f2c9d448f550895fe4b994e9d50fde4c735038
pkumar1618 <prashanthch943@gmail.com>
2026-05-20 14:57:14 +0530

    20-redirectors

---
+++ 20-redirectors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) # echo "20-redirectories.sh" | cut -d "." -f1
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0"
+
+CHECK_ROOT() {
+    if [ "$USERID" -ne 0 ]; then
+        echo -e "$R Please run this script with root privileges $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then 
+        echo -e "$2 ...$R FAILED $N" &>>$LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS $N" &>>$LOG_FILE
+    fi
+}
+
+CHECK_ROOT
+
+# sh 19-package is installing multiple packages git,mysql,postfix,nginx,redis
+
+
+for package in "$@"   # "$@" expands to all arguments passed to the script
+do
+    dpkg list installed "$package" &>/dev/null &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed. Going to install..." &>>$LOG_FILE
+        apt install -y "$package" &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed... nothing to do" &>>$LOG_FILE
+    fi
+done
